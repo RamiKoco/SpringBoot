5b15977a3ceddc641a503a3dafc7e64a4646c3f3
RamiKoco <ramikoco333433@gmail.com>
2024-01-09 00:31:39 +0300

    RequestMapping

--- pair-3/src/main/java/com/tobeto/pair3/controllers/SayHello.java
+++ pair-3/src/main/java/com/tobeto/pair3/controllers/SayHello.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path="/api")//class seviyesi için
+@RequestMapping(path="/api")//class seviyesi için...
 public class SayHello {
     @GetMapping(path="/hello")
     public String SayHello()
