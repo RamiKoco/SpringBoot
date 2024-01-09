2e5a82a57047d0b7f69521df7fd77ab25d1c9e92
RamiKoco <ramikoco333433@gmail.com>
2024-01-09 12:21:53 +0300

    PathVariable

--- pair-3/src/main/java/com/tobeto/pair3/controllers/SayHello.java
+++ pair-3/src/main/java/com/tobeto/pair3/controllers/SayHello.java
@@ -5,19 +5,29 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(path="/api")//class seviyesi için...
 public class SayHello {
-    @GetMapping(path="/hello")
-    public String SayHello()
-    {
-        return "Hello World!";
+//    @GetMapping(path="/hello")
+//    public String SayHello()
+//    {
+//        return "Hello World!";
+//    }
+//@PostMapping("/save")
+//    public String Num()
+//    {
+//        return "Saved!";
+//    }
+//    @DeleteMapping("/delete")
+//    public String delete(){
+//        return "Data deleted!";
+//    }
+//@GetMapping("/message/{m}")
+//    public String getMyData1(@PathVariable("m") String strName){
+//        return "My Name IS :"+strName;
+//    }
+    //OR
+    @GetMapping("/message/{strName}")
+    public String getMyData2(@PathVariable String strName){
+        return "My Name IS :"+strName;
     }
-@PostMapping("/save")
-    public String Num()
-    {
-        return "Saved!";
-    }
-    @DeleteMapping("/delete")
-    public String delete(){
-        return "Data deleted!";
-    }
+
 }
 
